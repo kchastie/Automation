5f6ad8ddcffc711703ea5e381ccc5d46a38c4f6b
Kyle Hastie <59584959+kchastie@users.noreply.github.com>
2022-10-28 14:42:22 -0600

    Update sisuTarget.js

--- cypress/e2e/crmtargets/sisuTarget.js
+++ cypress/e2e/crmtargets/sisuTarget.js
@@ -1,5 +1,4 @@
 /// <reference types= 'Cypress'/>
-
 //import { makeid } from "../crmsources/nameRandomize";
 //import { makePhone } from "../crmsources/phoneRandomize";
 Cypress.on("uncaught:exception", (err, runnable) => {
@@ -16,11 +15,7 @@ describe("Sisu lead validation", function () {
     cy.visit("https://my.sisu.co/transactions/records?reset_filters=True");
     cy.get('[tabindex="1"] > [data-col="agent_name"]').click();
     let today = new Date().toLocaleDateString();
-    cy.get(":nth-child(2) > .text-field").should("have.text", today);
-    cy.get('.nav > :nth-child(2) > ul > :nth-child(9) > a').click();
-   cy.get(':nth-child(11) > :nth-child(5)').should('have.text','API: RealSynch');
-    cy.get('.sidenav > ul > :nth-child(7) > a').click();
-    cy.get(':nth-child(1) > :nth-child(2) > .text-field').should('contain','Cypress');
-
+    cy.get(':nth-child(2) > .text-field').should('have.text',today);
+    cy.get('#last_name').should('have.value', 'Cypress');
   });
 });
